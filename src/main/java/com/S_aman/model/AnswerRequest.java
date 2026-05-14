6c8467376d210300baf87dbba41054c1d39cbfad
Kumar-Aman7974 <amanbth7974@gmail.com>
2026-05-14 22:28:34 +0530

    created getter and setter method

--- src/main/java/com/S_aman/model/AnswerRequest.java
+++ src/main/java/com/S_aman/model/AnswerRequest.java
@@ -5,11 +5,25 @@ import lombok.Data;
 
 import java.util.List;
 
-@Data
+
 public class AnswerRequest {
 
     private  String quizId; // which quiz are you answering?
     private List<String> answers; //User's selected answers
 
+    public List<String> getAnswers() {
+        return answers;
+    }
 
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+
+    public String getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(String quizId) {
+        this.quizId = quizId;
+    }
 }
---
+++ src/main/java/com/S_aman/service/QuizStoreService.java
@@ -0,0 +1,4 @@
+package com.S_aman.service;
+
+public class QuizStoreService {
+}
